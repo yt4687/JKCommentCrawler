94cd2f1478a7562150c2f1325333fa3815199f89
yt4687 <35659282+yt4687@users.noreply.github.com>
2021-03-18 13:52:51 +0900

    SQLへ書き込む機能を追加
    オリジナルの基本機能を変えずにSQLへ書き込む機能を追加。
    使わない場合の処理を現状追加していないので設定せず使うと落ちます

--- JKCommentCrawler.py
+++ JKCommentCrawler.py
@@ -14,6 +14,9 @@ import time
 import traceback
 import xml.dom.minidom as minidom
 import requests
+import MySQLdb
+from lxml import etree
+from MySQLdb.cursors import DictCursor
 
 import JKComment
 
@@ -44,6 +47,11 @@ def main():
     nicologin_mail = config.get('Default', 'nicologin_mail')
     nicologin_password = config.get('Default', 'nicologin_password')
     discord_webhookurl = config.get('Default', 'webhook_url')
+    sql_username = config.get('SQLSetting', 'SQL_USername')
+    sql_password = config.get('SQLSetting', 'SQL_password')
+    sql_dbname = config.get('SQLSetting', 'SQL_dbname')
+    sql_host = config.get('SQLSetting', 'SQL_host')
+    sql_port = config.get('SQLSetting', 'SQL_port')
 
     # 行区切り
     print('=' * shutil.get_terminal_size().columns)
@@ -130,8 +138,103 @@ def main():
             xml = xml.replace('<packet>\n', '').replace('</packet>', '').replace('<packet/>', '')
             return xml.rstrip()
 
+        # SQLに書き込みをする
+        def writesql(jikkyoid, sourcedata):
+            # ファイル書き込みの方はXMLの親要素まで消すので元データを自分で加工する
+            sourcedata = ET.tostring(sourcedata, encoding='UTF-8', pretty_print=True).decode('UTF-8').replace('>\n  ', '>\n')  # インデントを除去
+            root = etree.fromstring(sourcedata)
+        
+            # 接続するDBを定義
+            # シングルクォーテーションが邪魔するので消す(いい方法があったら直すかも、一回しか通さないからいいんじゃないかとも思う)
+            thost = sql_host.replace("'", "")
+            tuser = sql_username.replace("'", "")
+            tpasswd = sql_password.replace("'", "")
+            tdb = sql_dbname.replace("'", "")
+            connection = MySQLdb.connect(host = thost, user = tuser, passwd = tpasswd, db = tdb, port = int(sql_port), charset = 'utf8mb4')
+            cursor = connection.cursor(DictCursor)
+
+            # テーブルがない場合は作成する
+            cursor.execute('create table if not exists '+jikkyoid+' (thread varchar(50) not null, no int(6) not null, vpos int(20) not null, date int(20) not null, date_usec int(15) not null, mail varchar(50) not null, user_id varchar(100) not null, premium varchar(1) not null, anonymity varchar(1) not null, text varchar(10000) not null, primary key(thread, no));')
+
+            # 配列をセット
+            params = []
+            print('SQLデータベースへの書き込みを行っています...')
+            #実際の書き込み処理
+            for elem in root.iter('chat'):
+
+                # 登録があるかを確認
+                cursor.execute('SELECT * FROM ' + jikkyoid + ' WHERE thread=%s and no=%s;', (elem.get('thread'), elem.get('no')))
+
+                # 新規に登録する場合
+                if cursor.rowcount == 0:
+
+                    try:
+                        # vposが空のことがある
+                        if elem.get('vpos') == None:
+                            sqlvpos = 0
+                        else:
+                            sqlvpos = elem.get('vpos')
+                        # date_usecが空のことがある
+                        if elem.get('date_usec') == None:
+                            sqldate_usec = '0'
+                        else:
+                            sqldate_usec = elem.get('date_usec')
+                        # mailが空のことがある
+                        if elem.get('mail') == None:
+                            sqlmail = '0'
+                        else:
+                            sqlmail = elem.get('mail')
+                        # premium属性の処理
+                        if elem.get('premium') == "1":
+                            sqlpremium = "1"
+                        else:
+                            sqlpremium = "0"
+                        # anonymity属性の処理
+                        if elem.get('anonymity') == "1":
+                            sqlanonymity = "1"
+                        else:
+                            sqlanonymity = "0"
+
+                        # sql文を生成
+                        sql_insert = "INSERT INTO " + jikkyoid + " (thread, no, vpos, date, date_usec, mail, user_id, premium, anonymity, text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+                        sql_data = (elem.get('thread'), elem.get('no'), sqlvpos, elem.get('date'), sqldate_usec, sqlmail, elem.get('user_id'), sqlpremium, sqlanonymity, elem.text)
+                        
+                        # 配列に書き込み
+                        params.append(sql_data)
+                        
+                        #print(sql_insert, sql_data)
+                        # sqlを実行
+                        #cursor.execute(sql_insert, sql_data)
+                        # コミットする
+                        #connection.commit()
+
+                    except MySQLdb.Error as e:
+                        print('MySQLdb.Error: ', e)
+                        print('書き込めなかった項目 スレッド番号：' + elem.get('thread') + ' 連番：' + elem.get('no'))
+
+                    # 生成したデータをまとめて書き込み
+                    sql_insert = "INSERT INTO " + jikkyoid + " (thread, no, vpos, date, date_usec, mail, user_id, premium, anonymity, text) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+                    cursor.executemany(sql_insert,params)
+                    # コミットする
+                    connection.commit()
+                    #print(params)
+
+                # 既にデータがある場合    
+                elif cursor.rowcount == 1:
+                    #print('同一のデータが登録されているためスキップします スレッド番号：' + elem.get('thread') + ' 連番：' + elem.get('no'))
+                    pass
+
+                # それ以外、1件以上のデータがある場合
+                #else :
+                    #print('データに異常があります')
+                    #break
+            # セッションを閉じる
+            connection.close()
+
         # XML にフォーマット
         comment_xml = format_xml(comment_xmlobject)
+        # SQLに書き込み
+        writesql(jikkyo_id, comment_xmlobject)
 
         # ファイル名・フォルダ
         os.makedirs(f"{jkcomment_folder}/{jikkyo_id}/{date.strftime('%Y')}/", exist_ok=True)
